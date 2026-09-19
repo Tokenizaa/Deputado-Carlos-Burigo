9ae3c4a2cc44aef929211cb3a3db057cbcb541fa
Netto Farias <35587885+Tokenizaa@users.noreply.github.com>
2026-09-19 17:19:29 -0300

    fix(acervo): completar expressao condicional da fonte das votacoes

--- src/components/public/ActionsAndProjectsSection.tsx
+++ src/components/public/ActionsAndProjectsSection.tsx
@@ -278,7 +278,7 @@ export const ActionsAndProjectsSection: React.FC<ActionsAndProjectsSectionProps>
                             )}
                           </div>
                         </div>
-                      )}
+                      ) : null}
                     </div>
                   </details>
                 ))}
